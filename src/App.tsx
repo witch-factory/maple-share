2ad2eec545ff157a374231ebdd6e28ec879268c6
witch-factory <soakdma37@gmail.com>
2023-09-30 22:53:38 +0900

    react router dom install

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { Button } from './components/ui/button';
-
 function App() {
   return (
-    <div>
-      <h1>메이플 분배금 계산기</h1>
-      <Button>버튼</Button>
-      <Button className='bg-indigo-500'>버튼</Button>
-      <Button className='bg-pink-500'>버튼</Button>
-    </div>
+    <main className='p-10 flex justify-center'>
+      <h1 className='scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl'>
+        분배금 계산기
+      </h1>
+    </main>
   );
 }
 
