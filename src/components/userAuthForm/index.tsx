1677a067e61eb877fd50ab85636e299a97465663
witch-factory <soakdma37@gmail.com>
2023-10-13 01:41:23 +0900

    구글, 애플 로그인 버튼 추가

--- src/components/userAuthForm/index.tsx
+++ src/components/userAuthForm/index.tsx
@@ -18,7 +18,7 @@ function UserAuthForm() {
   };
 
   return (
-    <div className='grid gap-6'>
+    <div className='grid gap-4 w-80'>
       <form onSubmit={onSubmit}>
         <div className='grid gap-2'>
           <div className='grid gap-1'>
@@ -27,7 +27,7 @@ function UserAuthForm() {
             </Label>
             <Input
               id='party'
-              placeholder='단퐁회'
+              placeholder='파티명'
               type='text'
               autoCapitalize='none'
               autoComplete='organization'
@@ -62,8 +62,48 @@ function UserAuthForm() {
           </span>
         </div>
       </div>
-      <Button variant='outline' type='button' disabled={isLoading}>
-        Google 로그인
+      <Button className='w-full bg-[#4285F4] text-white' variant='outline'>
+        <div className='flex items-center justify-center'>
+          <svg
+            className=' w-5 h-5 mr-2'
+            fill='none'
+            height='24'
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth='2'
+            viewBox='0 0 24 24'
+            width='24'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <circle cx='12' cy='12' r='10' />
+            <circle cx='12' cy='12' r='4' />
+            <line x1='21.17' x2='12' y1='8' y2='8' />
+            <line x1='3.95' x2='8.54' y1='6.06' y2='14' />
+            <line x1='10.88' x2='15.46' y1='21.94' y2='14' />
+          </svg>
+          구글 로그인
+        </div>
+      </Button>
+      <Button className='w-full bg-black text-white space-y-4' variant='outline'>
+        <div className='flex items-center justify-center'>
+          <svg
+            className=' w-5 h-5 mr-2'
+            fill='none'
+            height='24'
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth='2'
+            viewBox='0 0 24 24'
+            width='24'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path d='M12 20.94c1.5 0 2.75 1.06 4 1.06 3 0 6-8 6-12.22A4.91 4.91 0 0 0 17 5c-2.22 0-4 1.44-5 2-1-.56-2.78-2-5-2a4.9 4.9 0 0 0-5 4.78C2 14 5 22 8 22c1.25 0 2.5-1.06 4-1.06Z' />
+            <path d='M10 2c1 .5 2 2 2 5' />
+          </svg>
+          애플 로그인
+        </div>
       </Button>
     </div>
   );
