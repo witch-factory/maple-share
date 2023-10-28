fcddd4bb594ec18f3b25f8dc2a18336e5069168f
witch-factory <soakdma37@gmail.com>
2023-10-29 02:28:04 +0900

    페이지 틀에 임시 페이지 링크들 추가

--- src/components/pageTemplate/index.tsx
+++ src/components/pageTemplate/index.tsx
@@ -1,7 +1,16 @@
+import { Link } from 'react-router-dom';
+
+import { Card } from '@/components/ui/card';
+
 function PageTemplate({ children }: {children: React.ReactNode}) {
   return (
     <main className='w-full h-screen flex flex-col justify-center items-center bg-center bg-cover bg-zinc-900'>
       {children}
+      <Card className='w-40 flex flex-col'>
+        <Link to='/'>메인</Link>
+        <Link to='/login'>로그인</Link>
+        <Link to='/register'>회원가입</Link>
+      </Card>
     </main>
   );
 }
