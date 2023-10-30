7602108f426c4161fd2e30a55e9156ffa6c70bac
witch-factory <soakdma37@gmail.com>
2023-10-30 23:01:46 +0900

    비밀번호 찾기 간단 페이지

--- src/pages/pw-inquiry/index.tsx
+++ src/pages/pw-inquiry/index.tsx
@@ -1,11 +1,90 @@
+import { useState } from 'react';
+
+import CardBox from '@/components/cardBox';
+import LabelledInput from '@/components/labelledInput';
 import PageTemplate from '@/components/pageTemplate';
+import { Button } from '@/components/ui/button';
+import {
+  Card, CardContent, CardHeader, CardTitle, CardDescription,
+} from '@/components/ui/card';
+
+type FormInfo = {
+  partyName: string;
+  email: string;
+};
 
 function PwInquiryPage() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [formInfo, setFormInfo] = useState<FormInfo>({
+    partyName: '',
+    email: '',
+  });
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+      alert('입력하신 이메일로 메일이 발송되었습니다. 메일함을 확인해주세요.');
+    }, 1000);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormInfo(prev => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   return (
     <PageTemplate>
-      <div className='flex flex-col items-center justify-center w-full h-full'>
-        <h1 className='text-5xl font-bold text-white'>비밀번호 찾기</h1>
-      </div>
+      <CardBox>
+        <Card className='w-[90%] mx-auto'>
+          <CardHeader className='space-y-1'>
+            <CardTitle>비밀번호 찾기</CardTitle>
+            <CardDescription>파티 생성 시 입력한 이메일을 통해 비밀번호를 찾아 드립니다.</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className='grid gap-4 w-full'>
+              <form onSubmit={onSubmit}>
+                <div className='grid gap-4'>
+                  <LabelledInput
+                    id='partyName'
+                    label='파티명'
+                    placeholder='파티명'
+                    type='text'
+                    autoCapitalize='none'
+                    autoComplete='organization'
+                    autoCorrect='off'
+                    disabled={isLoading}
+                    value={formInfo.partyName}
+                    onChange={handleChange}
+                    required
+                  />
+                  <LabelledInput
+                    id='email'
+                    label='이메일'
+                    placeholder='이메일'
+                    type='email'
+                    autoCapitalize='none'
+                    autoComplete='email'
+                    autoCorrect='off'
+                    disabled={isLoading}
+                    value={formInfo.email}
+                    onChange={handleChange}
+                    required
+                  />
+                  <Button type='submit' disabled={isLoading}>
+                    {isLoading ? '로딩 중...' : '비밀번호 찾기'}
+                  </Button>
+                </div>
+              </form>
+            </div>
+          </CardContent>
+        </Card>
+      </CardBox>
     </PageTemplate>
   );
 }
