80cbbe44d5f4f38d64d9828dc6f36922d56e3f6c
witch-factory <soakdma37@gmail.com>
2023-11-12 16:45:26 +0900

    로그인 시 메인 페이지로 이동 로직 구현

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import CardBox from '@/components/cardBox';
 import LabelledInput from '@/components/labelledInput';
@@ -9,10 +9,14 @@ import GoogleLoginButton from '@/components/socialLoginButton/google';
 import KakaoLoginButton from '@/components/socialLoginButton/kakao';
 import { Button } from '@/components/ui/button';
 import {
-  Card, CardContent, CardHeader, CardTitle, CardDescription,
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
 } from '@/components/ui/card';
 
-type FormInfo={
+type FormInfo = {
   partyName: string;
   password: string;
 };
@@ -23,15 +27,27 @@ function LoginPage() {
     partyName: '',
     password: '',
   });
+  const navigate = useNavigate();
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setIsLoading(true);
 
-    await fetch(`api/login?partyName=${formInfo.partyName}&password=${formInfo.password}`).then(res => res.json()).then(res => {
-      console.log(res);
-      setIsLoading(false);
-    });
+    await fetch(
+      `api/login?partyName=${formInfo.partyName}&password=${formInfo.password}`,
+    )
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return res.json() as Promise<FormInfo>;
+      })
+      .then(res => {
+        setIsLoading(false);
+        if (res.partyName === formInfo.partyName) {
+          navigate('/');
+        }
+      });
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -48,7 +64,9 @@ function LoginPage() {
         <Card className='w-[90%] mx-auto'>
           <CardHeader className='space-y-1'>
             <CardTitle className='text-2xl font-bold'>로그인</CardTitle>
-            <CardDescription>파티명과 비밀번호를 입력해 주세요.</CardDescription>
+            <CardDescription>
+              파티명과 비밀번호를 입력해 주세요.
+            </CardDescription>
           </CardHeader>
           <CardContent>
             <div className='space-y-4 w-full'>
@@ -84,7 +102,10 @@ function LoginPage() {
                 </div>
               </form>
               <Button className='w-full bg-zinc-500 text-white' type='button'>
-                <Link className='inline-block text-sm no-underline' to='/register'>
+                <Link
+                  className='inline-block text-sm no-underline'
+                  to='/register'
+                >
                   파티 만들기
                 </Link>
               </Button>
@@ -95,12 +116,18 @@ function LoginPage() {
               <Separator text='아이디/비밀번호 찾기' />
               <div className='w-full flex flex-row space-x-4'>
                 <Button className='w-full text-white p-0' type='button'>
-                  <Link className='flex w-full h-full text-sm no-underline justify-center items-center' to='/id-inquiry'>
+                  <Link
+                    className='flex w-full h-full text-sm no-underline justify-center items-center'
+                    to='/id-inquiry'
+                  >
                     파티명 찾기
                   </Link>
                 </Button>
                 <Button className='w-full text-white p-0' type='button'>
-                  <Link className='flex w-full h-full text-sm no-underline justify-center items-center' to='/pw-inquiry'>
+                  <Link
+                    className='flex w-full h-full text-sm no-underline justify-center items-center'
+                    to='/pw-inquiry'
+                  >
                     비밀번호 찾기
                   </Link>
                 </Button>
@@ -109,7 +136,6 @@ function LoginPage() {
           </CardContent>
         </Card>
       </CardBox>
-
     </PageTemplate>
   );
 }
